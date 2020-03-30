2e637dfcd5a13db5a3782f12065e686daaa09d34
DhananjayD <dugwekar@gmail.com>
2020-03-30 19:06:34 +0200

    first commit

---
+++ src/main/java/dev/payference/App.java
@@ -0,0 +1,13 @@
+package dev.payference;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
